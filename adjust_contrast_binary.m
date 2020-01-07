25965c06b8a2d117d4bb8b5f5449e58a75af5386
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2020-01-07 09:45:53 -0500

    Use to improve contrast of cells and correct for uneven illuminations

--- adjust_contrast_binary.m
+++ adjust_contrast_binary.m
@@ -1,19 +1,17 @@
 %Rhia Singh 
 %January 6, 2019 
-%Input: Provide the image that was pre-processed into the correct format. 
-%Output: Saves an image with the enhavnced contrast and smoothed
+%Input: Provide the image that was pre-processed into the correct format through image file prep function. 
+%Output: Saves an image with the enhanced contrast and smoothed
 %illuminations. 
 
 function contrastAdjusted = adjust_contrast_binary(img_processed)
+    
+    I_processed = imread(img_processed);
 %% Correct for the uneven illuminations in the image 
     se = strel('disk',12);
-    tophatFiltered = imtophat(img_processed,se);
-    %figure
-    %imshow(tophatFiltered,[]); 
+    tophatFiltered = imtophat(I_processed,se);
 
 %% Adjust the contrast in the image
     contrastAdjusted = imadjust(tophatFiltered);
-    imwrite(grayscale, 'grayscale.png');
-    %figure
-    %imshow(contrastAdjusted,[]);
+    imwrite(contrastAdjusted, 'contrast_adjust_grayscale.png');
 end
